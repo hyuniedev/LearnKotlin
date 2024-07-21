80e2434bf7a5d29804016b5cfbebd5b6aae077b0
NguyenHuuNien <huunien734@gmail.com>
2024-07-22 00:06:25 +0700

    + Add exam 1

--- Compose Knowledge Base/app/src/main/java/com/example/thcomposeknowledgebase/MainActivity.kt
+++ Compose Knowledge Base/app/src/main/java/com/example/thcomposeknowledgebase/MainActivity.kt
@@ -4,13 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.thcomposeknowledgebase.ui.theme.THComposeKnowledgeBaseTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +32,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             THComposeKnowledgeBaseTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(modifier = Modifier.fillMaxSize()) {
                 }
             }
         }
@@ -31,17 +40,77 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun GreetingText(modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = stringResource(id = R.string.title1),
+            modifier = Modifier
+                .padding(16.dp),
+            fontSize = 24.sp
+        )
+        Text(
+            text = stringResource(id = R.string.title2),
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.padding(
+                start = 16.dp,
+                end = 16.dp
+            ),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = stringResource(id = R.string.title3),
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
+}
+
+@Composable
+fun GreetingImage(modifier: Modifier = Modifier){
+    Image(
+        painter = painterResource(id = R.drawable.bg_compose_background),
+        contentDescription = null
     )
+}
+
+@Composable
+fun CreateTask1(modifier: Modifier = Modifier){
+    Column(verticalArrangement = Arrangement.Center) {
+        GreetingImage()
+        GreetingText()
+    }
+}
+
+@Composable
+fun CreateTask2(modifier: Modifier = Modifier){
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Image(painter = painterResource(
+            id = R.drawable.ic_task_completed),
+            contentDescription = null
+        )
+        Text(
+            text = stringResource(id = R.string.task),
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(top = 24.dp, bottom = 8.dp)
+        )
+        Text(
+            text = stringResource(id = R.string.nice),
+            fontSize = 16.sp
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     THComposeKnowledgeBaseTheme {
-        Greeting("Android")
+        CreateTask2()
     }
 }
