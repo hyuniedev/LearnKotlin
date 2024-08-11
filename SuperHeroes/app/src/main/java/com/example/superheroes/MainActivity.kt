c26e3a827d7f386e8796347f56a3d94bf7cf507b
NguyenHuuNien <huunien734@gmail.com>
2024-08-11 18:03:25 +0700

    + add app super Heroes

--- SuperHeroes/app/src/main/java/com/example/superheroes/MainActivity.kt
+++ SuperHeroes/app/src/main/java/com/example/superheroes/MainActivity.kt
@@ -4,13 +4,38 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.superheroes.entity.Hero
+import com.example.superheroes.model.HeroesRepository.heros
 import com.example.superheroes.ui.theme.SuperHeroesTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,6 +55,63 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun ShowListHero(modifier: Modifier = Modifier){
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Row(modifier = modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center) {
+                        Text(text = "Superheroes", style = MaterialTheme.typography.displayLarge)
+                    }
+                }
+            )
+        }
+    ) { it ->
+        LazyColumn(modifier = Modifier.fillMaxWidth(), contentPadding = it) {
+            items(heros){
+                    item ->
+                CardHero(item)
+                CardHero(item)
+            }
+        }
+    }
+}
+
+@Composable
+private fun CardHero(hero: Hero,modifier: Modifier = Modifier){
+    Card(
+        modifier = modifier
+            .padding(horizontal = 16.dp, vertical = 8.dp),
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(2.dp)
+    ) {
+        Row(modifier = Modifier
+            .padding(16.dp)
+            .height(72.dp)
+            .fillMaxWidth()) {
+            Column(modifier = Modifier.weight(2f)) {
+                Text(
+                    text = stringResource(id = hero.nameRes),
+                    style = MaterialTheme.typography.displaySmall,
+                )
+                Text(
+                    text = stringResource(id = hero.descriptionRes),
+                    style = MaterialTheme.typography.bodyLarge
+                )
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+            Image(
+                painter = painterResource(id = hero.imageRes), 
+                contentDescription = null,
+                modifier = Modifier.clip(MaterialTheme.shapes.small)
+            )
+        }
+    }
+}
+
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
@@ -42,6 +124,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     SuperHeroesTheme {
-        Greeting("Android")
+        ShowListHero()
     }
 }
